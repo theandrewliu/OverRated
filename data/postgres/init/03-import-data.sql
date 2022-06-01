5993004a8806e1d99ece1de1738d9671b65f8eb5
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-06-01 15:25:05 -0500

    detail and post api endpoints for profile

--- data/postgres/init/03-import-data.sql
+++ data/postgres/init/03-import-data.sql
@@ -7,5 +7,5 @@
 
 INSERT INTO public.profiles (username, password, first_name, last_name, location, date_of_birth, photo, about, height, job, education, gender, sexual_orientation, religion, ethnicity, pronouns)
     VALUES
-    ('user4',	'andrew', 'Andrew', 'Liu', 'Los Angeles', '07-22-1994', NULL, 'cool guy', '183', 'codemonkey', 'hack reactor', 'male', 'straight', 'none', 'Chinese', 'He'),
-    ('user3',	'yesenia', 'Yesenia', 'Ramirez', 'Austin', '04-14-1992', NULL, 'cool girl', '140', 'codemonkey', 'hack reactor', 'female', 'straight', 'none', 'Hispanic', 'She');	
+    ('user4','password', 'Andrew', 'Liu', 'Los Angeles', '07-22-1994', NULL, 'cool guy', '183', 'codemonkey', 'hack reactor', 'male', 'straight', 'none', 'Chinese', 'He'),
+    ('user3', 'password', 'Yesenia', 'Ramirez', 'Austin', '04-14-1992', NULL, 'cool girl', '140', 'codemonkey', 'hack reactor', 'female', 'straight', 'none', 'Hispanic', 'She');	
