6988c23792f4432bbafcbf9228356462535c4d21
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-06-02 17:45:23 -0500

    update

--- data/postgres/init/03-import-data.sql
+++ data/postgres/init/03-import-data.sql
@@ -9,3 +9,5 @@ INSERT INTO public.profiles (username, email, password, first_name, last_name, l
     VALUES
     ('user4','andrew@gmail.com', 'password', 'Andrew', 'Liu', 'Los Angeles', '07-22-1994', NULL, 'cool guy', '183', 'codemonkey', 'hack reactor', 'male', 'straight', 'none', 'Chinese', 'He'),
     ('user3', 'yesenia@gmail.com', 'password', 'Yesenia', 'Ramirez', 'Austin', '04-14-1992', NULL, 'cool girl', '140', 'codemonkey', 'hack reactor', 'female', 'straight', 'none', 'Hispanic', 'She');	
+
+INSERT INTO public.matches (user1, user2, message)
