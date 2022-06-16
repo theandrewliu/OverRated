c4a260b8a9754a12122f949dde9f682354f04154
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-06-16 18:48:36 -0500

    message detail

--- ghi/app/src/Profiles/MessageDetail.js
+++ ghi/app/src/Profiles/MessageDetail.js
@@ -13,23 +13,36 @@ class MessageDetail extends React.Component {
         super(props);
         this.state = {
             messages: {'messages': []},
+            profiles: "",
             redirect: false,
         };
     }
 
     componentDidMount() {
         this.getMyMessages();
+        this.getTargetInfo();
     }
 
+    async getTargetInfo() {
+        const url = `${process.env.REACT_APP_API_HOST}/api/profiles/${this.props.target_id}`;
+        const response = await fetch(url, {
+            credentials: 'include',
+        });
+        if (response.ok) {
+            this.setState({
+                profiles: await response.json(),
+            });
+        } else if (response.status === 401) {
+            this.setState({redirect: true})
+        }
+    }
 
     async getMyMessages() {
         const url = `${process.env.REACT_APP_API_HOST}/api/messages/${this.props.target_id}`;
         const response = await fetch(url, {
             credentials: 'include',
         });
-        console.log("hello here")
         if (response.ok) {
-            console.log("response is ok")
             this.setState({
                 messages: await response.json(),
             });
@@ -44,6 +57,7 @@ class MessageDetail extends React.Component {
             <h1>Chat with -Name Here-</h1>
             {/* {console.log("inside return statement")} */}
             {console.log("first", this.state.messages.messages)}
+            {console.log("did this work", this.state.profiles)}
             <div>
                 {this.state.messages.messages.map(message => {
                     return (<>
@@ -54,11 +68,16 @@ class MessageDetail extends React.Component {
                         <div>
                             Message: {message.message}
                         </div>
+                        <div>
+                            Photo: {this.state.profiles.photo}
+                        </div>
+                        <div>
+                            Name: {this.state.profiles.first_name} {this.state.profiles.last_name}
+                        </div>
                         </>
                     )
                 })}
-            </div>
-            {/* <p>{this.state.messages.message}</p> */}
+            </div> 
             </>
         );
     }
