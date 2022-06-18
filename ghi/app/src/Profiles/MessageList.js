4bcd3d1a572fc308b21b08dc32984aaf4c801816
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-06-17 20:42:53 -0500

    finally getting somewhere

--- ghi/app/src/Profiles/MessageList.js
+++ ghi/app/src/Profiles/MessageList.js
@@ -1,107 +1,38 @@
 import React from "react";
-import { Navigate, Link, useParams } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
 
-function MessagesListGrabber(){
-  const params = useParams();
-  const target_id = params.id;
-  return <MessageList target_id = {target_id}></MessageList>
-}
 
 class MessageList extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      messages: {'messages': []},
-      target: "",
-      user: "",
-      message: "",
-      redirect: false,
-  };
+      super(props);
+      this.state = {
+          messages: {'messages': []},
+          matches: {'matches': []},
+          user: "",
+          message: "",
+          redirect: false,
+      };
   }
 
-  // async getMyData() {
-    
-  //   let url = `${process.env.REACT_APP_API_HOST}/api/messages`;
-  //   let messages = null 
-  //   let user = null
-  //   let matches = null
-  //   let target = null
-  //   let response = await fetch(url, {
-  //     credentials: 'include',
-  //   });
-  //   if (response.ok) {
-  //       messages = await response.json();
-  //   } else if (response.status === 401) {
-  //     this.setState({ redirect: true })
-  //   }
-  //   console.log("set", messages)
-  //   url = `${process.env.REACT_APP_API_HOST}/api/my-matches`;
-  //   response = await fetch(url, {
-  //     credentials: 'include',
-  //   });
-  //   if (response.ok) {
-  //       matches = await response.json();
-  //   } else if (response.status === 401) {
-  //     this.setState({ redirect: true })
-  //   }
-  //   console.log("turbo", matches)
-  //   url = `${process.env.REACT_APP_API_HOST}/api/profiles/mine`;
-  //   response = await fetch(url, {
-  //     credentials: 'include',
-  //   });
-  //   if (response.ok) {
-  //       user = await response.json();
-  //   } else if (response.status === 401) {
-  //     this.setState({ redirect: true })
-  //   }
-  //   console.log("userinfopull", user)
-
-  //   if(messages && matches){
-  //     for(let message of messages){
-  //       for(let match of matches) {
-  //         console.log("match", match)
-  //         console.log("message", message)
-  //         if (message.match_id === match.match_id){
-  //           target = match.id
-  //           console.log("target in loop", target)
-
-  //         }
-  //       // target = matches.find(match => match.match_id === message.match_id)
-        
-  //       }
-  //     }
-  //     console.log("target", target)
-  //     this.setState({ messages })
-      
-  //   }
-    
-  // }
   async componentDidMount() {
-    const targetURL = `${process.env.REACT_APP_API_HOST}/api/profiles/${this.props.target_id}`;
-    const messagesURL = `${process.env.REACT_APP_API_HOST}/api/messages/${this.props.target_id}`;
-    const userURL = `${process.env.REACT_APP_API_HOST}/api/profiles/mine/`;
+      const matchesURL = `${process.env.REACT_APP_API_HOST}/api/my-matches/`;
+      const messagesURL = `${process.env.REACT_APP_API_HOST}/api/messages/`;
+      const userURL = `${process.env.REACT_APP_API_HOST}/api/profiles/mine/`;
 
-    const targetResponse = await fetch(targetURL, {credentials: 'include'});
-    const messageResponse = await fetch(messagesURL, {credentials: 'include'});
-    const userResponse = await fetch(userURL, {credentials: 'include'});
-    
-    if (targetResponse.ok && messageResponse.ok && userResponse.ok) {
-        this.setState({
-            target: await targetResponse.json(),
-            messages: await messageResponse.json(),
-            user: await userResponse.json(),
-        });
-    } else if (targetResponse.status === 401 || messageResponse.status === 401 || userResponse.status === 401) {
-        this.setState({redirect: true})
-    }
-}
-
-
-  // componentDidMount() {
-  //   this.getMyData();
-
-  // }
-
+      const matchesResponse = await fetch(matchesURL, {credentials: 'include'});
+      const messageResponse = await fetch(messagesURL, {credentials: 'include'});
+      const userResponse = await fetch(userURL, {credentials: 'include'});
+      
+      if (matchesResponse && messageResponse.ok && userResponse.ok) {
+          this.setState({
+              matches: await matchesResponse.json(),
+              messages: await messageResponse.json(),
+              user: await userResponse.json(),
+          });
+      } else if (matchesResponse.status === 401 || messageResponse.status === 401 || userResponse.status === 401) {
+          this.setState({redirect: true})
+      }
+  }
 
   render() {
     if (this.state.redirect === true) {
@@ -109,11 +40,13 @@ class MessageList extends React.Component {
     }
     return (
       <>
-      {console.log("messages", this.state.messages.messages)}
-      {console.log("target", this.state.target)}
-      {console.log("user", this.state.user)}
         <h1><b>Your Messages</b></h1>
-        {console.log("state messages", this.state.messages)}
+
+        {console.log("corey test:", this.state)}
+        {console.log("matches", this.state.matches.matches)}
+        {console.log("messages", this.state.messages.messages)}
+        {console.log("user", this.state.user)}
+
         <div className='message-layout'>{this.state.messages.messages.map(message => {
           console.log("message map", message)
 
@@ -121,8 +54,6 @@ class MessageList extends React.Component {
           if (message.photo === null) {
             photo = "/images/blank-profile-pic.png"
           }
-
-          
           return (
 
             <div className="message-card" key={message.match_id}>
@@ -151,4 +82,4 @@ class MessageList extends React.Component {
   }
 }
 
-export default MessagesListGrabber;
+export default MessageList;
