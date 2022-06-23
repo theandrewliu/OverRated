e6824fc27b847231e144620c44223e942a111aec
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-06-22 19:23:21 -0500

    profile form, login form, account form working

--- ghi/app/src/Accounts/AccountForm.js
+++ ghi/app/src/Accounts/AccountForm.js
@@ -13,15 +13,40 @@ class AccountForm extends React.Component {
             password: "",
             verify_password: "",
             error: "",
+            redirect: false,
+            profile: ""
     };
 
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleFirst_nameChange = this.handleFirst_nameChange.bind(this);
         this.handleLast_nameChange = this.handleLast_nameChange.bind(this);
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleVerify_PasswordChange = this.handleVerify_PasswordChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    async componentDidMount() {
+        const url = `${process.env.REACT_APP_API_HOST}/api/profiles/mine`;
+        const response = await fetch(url, {
+            credentials: 'include',
+        });
+        
+        if (response.ok) {
+        this.setState({
+            profile: await response.json(),
+        });
+        this.setState({
+            email: this.state.profile.email,
+            username: this.state.profile.username,
+            first_name: this.state.profile.first_name,
+            last_name: this.state.profile.last_name,
+        })
+        console.log("this is the state", this.state)
+        console.log("this is username if exists", this.state.username)
+        }else if (response.status === 401){
+        this.setState({redirect: true})
+        }
     }
 
 
@@ -29,6 +54,10 @@ class AccountForm extends React.Component {
         event.preventDefault();
         const data = {...this.state};
         console.log("Taco Taco", data);
+        delete data.redirect
+        delete data.error
+        delete data.profile
+
         const url = `${process.env.REACT_APP_API_HOST}/api/accounts/myself`;
         const fetchConfig = {
             method: "PUT",
@@ -44,14 +73,9 @@ class AccountForm extends React.Component {
             const accountform = await response.json();
             console.log("Taco Bueno", accountform);
             this.setState({
-                email: '',
-                username: '',
-                first_name: '',
-                last_name: '',
-                password: '',
-                verify_password: '',
-                error: '',
+                redirect: true
             });
+            
             if (response.ok) {
                 this.setState({
                     profile: await response.json(),
@@ -66,10 +90,7 @@ class AccountForm extends React.Component {
         const value = event.target.value;
         this.setState({ email: value });
     }
-    handleUsernameChange(event) {
-        const value = event.target.value;
-        this.setState({ username: value });
-    }
+
     handleFirst_nameChange(event) {
         const value = event.target.value;
         this.setState({ first_name: value });
@@ -91,15 +112,13 @@ class AccountForm extends React.Component {
         return this.state.password.length >= 8 &&
                this.state.password === this.state.verify_password &&
                this.state.email &&
-               this.state.username &&
                this.state.first_name &&
-               this.state.last_name &&
-               this.state.location;
+               this.state.last_name
     }
 
     render(){
-        if (this.props.token) {
-            return <Navigate to="/my_profile" />;
+        if(this.state.redirect === true){
+            return <Navigate to ='/my_profile'/>;
         }
         return(
             <div className="row">
@@ -109,52 +128,29 @@ class AccountForm extends React.Component {
                     <h1>Account Settings</h1>
                     <hr/>
                     <br></br>
+                    <h3>Username: {this.state.profile.username}</h3>
                     <form onSubmit={this.handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input onChange={this.handleEmailChange} value={this.state.email} placeholder="Email" required type="email" name="email" id="email" className="form-control" />
-                            <label htmlFor="email">Email</label>
-                        <button className="btn btn-primary">Edit</button>
-                    </div>
-                    </form>
-
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input onChange={this.handleLocationChange} value={this.state.location} placeholder="Location" required type="text" name="location" id="location" className="form-control" />
-                            <label htmlFor="location">Location</label>
-                        <button className="btn btn-primary">Edit</button>
-                    </div>
-                    </form>
-
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input onChange={this.handleFirst_nameChange} value={this.state.first_name} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
-                            <label htmlFor="first_name">First Name</label>
-                        <button className="btn btn-primary">Edit</button>
-                    </div>
-                    </form>
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input onChange={this.handleLast_nameChange} value={this.state.last_name} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
-                            <label htmlFor="last_name">Last Name</label> 
-                        <button className="btn btn-primary">Edit</button>
-                    </div>
-                    </form>
-
-                    <form onSubmit={this.handleSubmit}>
-                    <div className="form-floating mb-3">
-                            <input onChange={this.handleUsernameChange} value={this.state.username} placeholder="Username" required type="text" name="username" id="username" className="form-control" />
-                                <label htmlFor="username">Username</label>
-                            <button className="btn btn-primary">Edit</button>
+                        <label htmlFor="email">Email</label>
+                        <div className="form-floating mb-3">
+                            <input onChange={this.handleEmailChange} value={this.state.email} placeholder="placeholder there" required type="email" name="email" id="email" className="form-control" />
                         </div>
-                    </form>
-
-                        <form onSubmit={this.handleSubmit}>
+                    
+                        <label htmlFor="first_name">First Name</label>
+                        <div className="form-floating mb-3">
+                            <input onChange={this.handleFirst_nameChange} value={this.state.first_name} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
+                        </div>
+                    
+                        <label htmlFor="last_name">Last Name</label> 
+                        <div className="form-floating mb-3">
+                            <input onChange={this.handleLast_nameChange} value={this.state.last_name} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
+                        </div>
+                    
                         <div className="form-floating mb-3" >
                             <input onChange={this.handlePasswordChange} value={this.state.password} 
                                 placeholder="Password" required type="password" name="password" id="password" />
                             <input onChange={this.handleVerify_PasswordChange} value={this.state.verify_password} 
                                 placeholder="Verify Password" required type="password" name="verify-password" id="verify-password" />
-                            <button disabled={!this.validForm()} className="btn btn-primary">Edit</button>
+                            <button disabled={!this.validForm()} className="btn btn-primary">Update</button>
                         </div>
                     </form>
                 </div>
--- ghi/app/src/Accounts/LoginForm.js
+++ ghi/app/src/Accounts/LoginForm.js
@@ -19,6 +19,11 @@ class LoginForm extends React.Component{
         e.preventDefault()
         const error = await this.props.login(this.state.username, this.state.password);
         this.setState({ error: error })
+        this.setState({
+            username: '',
+            password: '',
+            showPassword: false,
+        });
     }
 
     validForm() {
@@ -30,39 +35,45 @@ class LoginForm extends React.Component{
             return <Navigate to="/my_profile" />;
         }
         return(
-        <section className='login'>
-        <div className="row_login">
-            <div className="offset-3 col-6" id="wrapper" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                <div className="shadow p-4 mt-4" id="outbox" >
-                    <div dangerouslySetInnerHTML={{__html: this.state.error}} />
-                    <form onSubmit={this.handleSubmit} id="create-form" >
-                        <h1>Log In</h1>
-                        <hr/>
-                    
-                        <div className="form-floating mb-3">
-                            <input type='text' id="loginpage" name='username' placeholder='Username' 
-                                required onChange={this.handleChange} />
-                        </div>
-                        <div className="form-floating mb-3" >
-                            <div className='loginpass'>
-                            <input type='password' id="loginpagepass" name='password' placeholder='Password' 
-                                required onChange={this.handleChange} />
-                            <button className='input-group-text bg-dark text-light'
-                                    onClick={() => this.setState({showPassword: !this.state.showPassword})}>Show
-                            </button>
+    
+        <div className='login'>
+            <div className="row_login">
+                <div className="offset-3 col-6" id="wrapper" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                    <div className="shadow p-4 mt-4" id="outbox" >
+                        <div dangerouslySetInnerHTML={{__html: this.state.error}} />
+                        <form onSubmit={this.handleSubmit} id="login-form" >
+                            <h1>Log In</h1>
+                            <hr/>
+                        
+                            <div className="form-floating mb-3">
+                                <input type='text' id="username" name='username' placeholder='Username' 
+                                    required onChange={this.handleChange} />
                             </div>
-                        </div>
-                        <hr/>
-                        <button onSubmit={this.handleSubmit} id='login_button' 
-                            disabled={!this.validForm()} value="Submit" 
-                            form="create-form" type="submit" >Log In</button>
+                            <div className="form-floating mb-3" >
+                                <input type={this.state.showPassword ? "text" : "password"} id="loginpagepass" name='password' placeholder='Password' 
+                                    required onChange={this.handleChange} />
+                                <button className='input-group-text bg-dark text-light' type="button"
+                                    onClick={() => this.setState({showPassword: !this.state.showPassword})}>Show Password
+                                </button>
+                            </div>
+                            <hr/>
+                            <br></br>
+                            <div>
+                                <button 
+                                    type="submit"
+                                    disabled={!this.validForm()} 
+                                    className="btn btn-primary" form="login-form">Log In
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
-            </section>
+        </div>
+       
         )
     }
+       
 }
 
 
