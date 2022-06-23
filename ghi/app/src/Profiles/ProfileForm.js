214374791860da411d97db02e30f8135cf8b87b8
Corey Edwards <coreydanieledwards@gmail.com>
2022-06-22 17:30:42 -0700

    end of day

--- ghi/app/src/Profiles/ProfileForm.js
+++ ghi/app/src/Profiles/ProfileForm.js
@@ -170,6 +170,7 @@ class ProfileForm extends React.Component {
     }
     handlePhotoChange(event) {
         const value = event.target.value;
+        console.log(value)
 
         const andrew = event.target.files[0];
 
