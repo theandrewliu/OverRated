48f3d184b707e745da7ba9f63cb262b17ff745bf
Jdawg <j.max.mao@gmail.com>
2022-06-23 13:39:37 -0500

    last test working

--- profiles_api/tests/test_accounts.py
+++ profiles_api/tests/test_accounts.py
@@ -8,33 +8,18 @@ from main import app
 
 
 class AccountInfo(TestCase):
-    def get_all_profiles(self, user_id, page: int=0):
-        return 1, [
-           [10, "test", "test", "test", "test", "test"]
+    def update_account(self, id, username, email, password, first_name, last_name):
+        return  [
+           10, "test", "test", "test", "test"
         ]
 
 async def override_get_fake_user():
   return {'id': 1, 'username': 'TailsPrower', 'email': 'Tails@Sonic.com', 
-    "first_name": 'Tails', "last_name": "Prower", "password": "lovesonic"}
+    "full_name": "Tails Prower"}
 
 async def override_update_account():
-  return {'id': 1, 'username': 'TailsSonic', 'email': 'TailsheartSonic@Sonic.com', 
-    "first_name": 'Tails', "last_name": "Sonic", "password": "lovesonic"}
-
-async def override_get_account():
-    return  {
-  "page_count": 0,
-  "profiles": [
-    {
-      "id": 0,
-      "username": "string",
-      "email": "string",
-      "first_name": "string",
-      "last_name": "string",
-      "password": "string",
-    }
-  ]
-}
+  return {'id': 1, 'username': 'TailsPrower', 'email': 'TailsheartSonic@Sonic.com', 
+    "first_name": 'Tails', "last_name": "Sonic"}
 
 
 app.dependency_overrides[update_account] = override_update_account
@@ -42,12 +27,21 @@ app.dependency_overrides[get_current_user] = override_get_fake_user
 
 client = TestClient(app)
 
-def test_profile_list():
+def test_update_account():
     app.dependency_overrides[ProfileQueries] = AccountInfo
     app.dependency_overrides[get_current_user] = override_get_fake_user
-    r = client.get("/api/accounts/myself")
-    # d = r.json()
+    r = client.put("/api/accounts/myself", json={
+      "username": "string",
+      "email": "string",
+      "password": "string",
+      "first_name": "string",
+      "last_name": "string"
 
+    })
+    d = r.json()
+    print("today", d)
     assert r.status_code == 200
 
     app.dependency_overrides = {}
+
+
